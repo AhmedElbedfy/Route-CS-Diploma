4eee4573d355bebae35cd707d382ad66bb6dab30
Ahmed Khaled <ahmed.khaled.hussain@gmail.com>
2024-01-31 20:16:17 +0200

    Add: 5th assignment file

--- CppCourse/assignments/Ahmed Khaled-CS C20 Friday 4PM-Maddi-4th assignments.cpp
+++ CppCourse/assignments/Ahmed Khaled-CS C20 Friday 4PM-Maddi-4th assignments.cpp
@@ -1,244 +1,46 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-/* ---------------- 2D Array Functions ---------------- */
-
-// Calculates and prints the sum of diagonal elements in a 3x3 array
-void Q_Num_12() {
-	cout << endl << "----- Question Number 12" << endl;
-
-	// create 2d array
-	int arr[3][3] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-	int counter = 0;
-
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			if (i == j) {
-				counter += arr[i][j];
-			}
-		}
+int factorial(int num) {
+	if (num == 1) {
+		return 1;
 	}
 
-	cout << counter << endl;
+	return factorial(num - 1) * num;
 }
 
-// Adds corresponding elements of two 3x3 arrays and prints the result
-void Q_Num_13() {
-	cout << endl << "----- Question Number 13" << endl;
-
-	// Input elements for the first array
-	int firstArray[3][3];
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			cin >> firstArray[i][j];
-		}
+char lowerCase(char zChar) {
+	if (zChar >= 65 && zChar <= 90) {
+		return zChar + 32;
 	}
-
-	// Input elements for the 2nd array
-	int secoundArray[3][3];
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			cin >> secoundArray[i][j];
-		}
-	}
-
-	// Calculates and prints the sum of each row in a 3x3 array
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			cout << firstArray[i][j] + secoundArray[i][j] << " ";
-		}
-		cout << endl;
-	}
+	return zChar;
 }
 
-
-// Calculates and prints the sum of each row in a 3x3 array
-void Q_Num_14() {
-	cout << endl << "----- Question Number 14" << endl;
-
-	int arr[3][3];
-
-	// Input elements for the array
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			cin >> arr[i][j];
-		}
+string lowerCaseSentence(string zString) {
+	for (int i = 0; i < zString.length(); i++) {
+		zString[i] = lowerCase(zString[i]);
 	}
-
-	// Calculate and print row sums
-	for (int i = 0; i < 3; i++) {
-		int counter = 0;
-		for (int j = 0; j < 3; j++) {
-			counter += arr[i][j];
-		}
-		cout << "Sum of row " << i + 1 << " = " << counter << endl;
-	}
-
-}
-
-
-/* ---------------- Function Assignments ---------------- */
-
-// Prints a given number
-void Q_Num_1_Functions(int number) {
-	cout << endl << "----- Question Number 1 in Functions" << endl;
-	cout << number;
-}
-
-// Calculates and returns the average of three numbers
-float Q_Num_2_Functions(int i, int j, int k) {
-	cout << endl << "----- Question Number 2 in Functions" << endl;
-	return ((i + j + k) / 3.0);
-}
-
-// Finds the maximum and minimum of two numbers
-int maxNumber(int a, int b) {
-	if (a > b) {
-		return a;
-	}
-	else {
-		return b;
-	}
-}
-
-int minNumber(int a, int b) {
-	if (b > a) {
-		return a;
-	}
-	else {
-		return b;
-	}
-}
-
-// Finds the maximum and minimum of two input numbers
-void Q_Num_3_Functions() {
-	cout << endl << "----- Question Number 3 in Functions" << endl;
-
-	int n1, n2;
-	cin >> n1;
-	cin >> n2;
-
-
-	cout << "Max number is: " << maxNumber(n1, n2) << endl;
-	cout << "Min number is: " << minNumber(n1, n2) << endl;
-}
-
-// Determines whether a given number is even or odd.
-void Q_Num_4_Functions() {
-	cout << endl << "----- Question Number 4 in Functions" << endl;
-
-	int num;
-	cin >> num;
-
-	if (num % 2 == 0) {
-		cout << "Number " << num << " is even." << endl;
-	}
-	else {
-		cout << "Number " << num << " is odd." << endl;
-	}
-}
-
-// Checks if a given number is prime.
-bool Q_Num_5_Functions() {
-	cout << endl << "----- Question Number 5 in Functions" << endl;
-
-	int number;
-	cin >> number;
-
-	if (number == 1) {
-		return false;
-	}
-	else {
-		int counter = 0;
-		for (int i = 1; i <= number; i++) {
-			if (number % i == 0) {
-				counter++;
-			}
-		}
-		if (counter == 2) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-
-
-}
-
-// Calculates and prints the cube of a given number.
-void Q_Num_6_Functions(int num) {
-	cout << endl << "----- Question Number 6 in Functions" << endl;
-	cout << "the cube of number: " << num << " is " << (num * num * num) << endl;
-}
-
-// Calculates and prints the diameter, circumference, and area of a circle.
-void Q_Num_7_Functions(float radius) {
-	cout << endl << "----- Question Number 7 in Functions" << endl;
-	float diameter = radius * 2;
-	float circumference = 2 * 3.14159 * radius;
-	float area = 3.14159 * radius * radius;
-
-	cout << "Diameter = " << diameter << " units" << endl;
-	cout << "Circumference = " << circumference << " units" << endl;
-	cout << "Area = " << area << " sq. units" << endl;
-
-}
-
-// Prints all factors of a given number.
-void Q_Num_8_Functions(int num) {
-	cout << endl << "----- Question Number 8 in Functions" << endl;
-
-	for (int i = 1; i <= num; i++) {
-		if (num % i == 0) {
-			cout << i << endl;
-		}
-	}
+	return zString;
 }
 
 
 int main()
 {
-	/* ---------------- 2D Array Functions ---------------- */
+	// 1st Question of the assignment
+	// cout << factorial(5);
 
-	// Calculates and prints the sum of diagonal elements in a 3x3 array
-	Q_Num_12();
-
-	// Adds corresponding elements of two 3x3 arrays and prints the result
-	Q_Num_13();
-
-	// Calculates and prints the sum of each row in a 3x3 array
-	Q_Num_14();
+	// 2nd Question of the assignment
 
 
-	/* ---------------- Function Assignments ---------------- */
+	// 3rd Question of the assignment
+	// cout << lowerCase('b') << endl;
+	// cout << lowerCaseSentence("AbCD") << endl;
 
-	// Prints a given number	
-	Q_Num_1_Functions(5);
-	
-	// Calculates and returns the average of three numbers
-	cout << Q_Num_2_Functions(1, 2, 3);
+	// 4th Question of the assignment
 
-	// Finds the maximum and minimum of two input numbers
-	Q_Num_3_Functions();
 
-	// Determines whether a given number is even or odd.
-	Q_Num_4_Functions();
-
-	// Checks if a given number is prime.
-	cout << Q_Num_5_Functions();
-
-	// Calculates and prints the cube of a given number.
-	Q_Num_6_Functions(5);
-
-	// Calculates and prints the diameter, circumference, and area of a circle.
-	Q_Num_7_Functions(10);
-
-	// Prints all factors of a given number.
-	Q_Num_8_Functions(25);
 
 	return 0;
 }
-
-
-
--- CppCourse/cpp-vs/cpp-vs.cpp
+++ CppCourse/cpp-vs/cpp-vs.cpp
@@ -1,20 +1,58 @@
-// cpp-vs.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+int factorial(int num) {
+	if (num == 1) {
+		return 1;
+	}
+	
+	return factorial(num - 1) * num;
+}
+
+char lowerCase(char zChar) {
+	if (zChar >= 65 && zChar <= 90) {
+		return zChar + 32;
+	}
+	return zChar;
+}
+
+string lowerCaseSentence(string zString) {
+	for (int i = 0; i < zString.length(); i++) {
+		zString[i] = lowerCase(zString[i]);
+	}
+	return zString;
+}
+
+bool isDigit(string zString) {
+	for (int i = 0; i < zString.length(); i++) {
+		if (zString[i] >= 0 && zString[i] <= 9) {
+			continue;
+		}
+		else {
+			return false;
+		}
+	}
+	return true;
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// 1st Question of the assignment
+	// cout << factorial(5);
+
+	// 2nd Question of the assignment
+
+
+	// 3rd Question of the assignment
+	// cout << lowerCase('b') << endl;
+	// cout << lowerCaseSentence("AbCD") << endl;
+
+	// 4th Question of the assignment
+	cout << isDigit("2342") << endl;
+	
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
