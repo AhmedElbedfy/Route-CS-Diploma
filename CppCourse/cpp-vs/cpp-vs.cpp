f662a0299ae4fff8b4a057bbc742c5bde28344ec
Ahmed Khaled <ahmed.khaled.hussain@gmail.com>
2024-02-04 01:43:07 +0200

    Add: Assignment 6 first Question

--- CppCourse/cpp-vs/cpp-vs.cpp
+++ CppCourse/cpp-vs/cpp-vs.cpp
@@ -1,12 +1,59 @@
 #include <iostream>
 #include <string>
 
+
 using namespace std;
+
+class Circle {
+private:
+	double radius;
+	string color;
+
+public:
+	Circle() {
+		radius = 1;
+		color = "red";
+	}
+	Circle(double r) {
+		radius = r;
+	}
+	Circle(double r, string c) {
+		radius = r;
+		color = c;
+	}
+	
+	double getRadius() {
+		return radius;
+	}
+	string getColor() {
+		return color;
+	}
+
+	void setRadius(double r) {
+		radius = r;
+	}
+
+	void setColor(string c) {
+		color = c;
+	}
+
+	double getArea() {
+		double pi = 3.14159265358979323846;
+		return pi * pow(radius, 2);
+	}
+};
 
 
 int main()
 {
-
+	Circle c1;
+	cout << c1.getRadius() << endl;
+	cout << c1.getColor() << endl;
+	cout << c1.getArea() << endl;
+	Circle c2(15, "black");
+	cout << c2.getRadius() << endl;
+	cout << c2.getColor() << endl;
+	cout << c2.getArea() << endl;
 
 	return 0;
 }
